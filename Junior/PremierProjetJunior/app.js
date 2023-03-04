a0bb7d9303e80ff74bbfb1629fcda3635cb765f5
Robloblo <108494251+Robloblo@users.noreply.github.com>
2023-03-04 17:21:37 +0100

    quasiiiii finiiiiii

--- Junior/PremierProjetJunior/app.js
+++ Junior/PremierProjetJunior/app.js
@@ -1,19 +1,51 @@
-let chiffre = document.querySelector(".form-num > input");
+let liste = document.querySelectorAll(".formulaire form div input");
+
+let input1 = liste[0];
+let input2 = liste[1];
+let input3 = liste[2];
+let input4 = liste[3];
+let input5 = liste[4];
+
 var chiffre_stock = "";
 
 const cart = "0000 0000 0000 0000";
+const nom = "------------------------------";
+const secret = "000";
 
-chiffre.addEventListener("input",handleReponse);
+input1.addEventListener("input",function(event){
+    handleReponse(event,".form-name > input",".card-front-info p:nth-child(1)",nom);
+});
+input2.addEventListener("input",function(event){
+    handleReponse(event,".form-num > input",".card-front-num p", cart);
+});
+input3.addEventListener("input",function(event){
+    handleReponse(event,".form-name > input",".card-front-info p:nth-child(2)");
+});
+input4.addEventListener("input",function(event){
+    handleReponse(event,".form-date >div:nth-child(2) > div > input",".texte-card-back p");
+});
+input5.addEventListener("input",function(event){
+    handleReponse(event,".num-secret > input",".texte-card-back p",secret);
+});
+
+console.log(liste);
+function handleReponse(event,debut,arriver,la_constante){
+    let nouvelle_valeur = "";
+        let chiffre = document.querySelector(debut);
+        for(let i = 0; i <= chiffre.value.length ; i++){
+            if(chiffre.value.charAt(i) != " "){
+                nouvelle_valeur += chiffre.value.charAt(i);
+            }
+            
+        chiffre_stock = nouvelle_valeur.substring(0,nouvelle_valeur.length-1);
+    }
     
-function handleReponse(event){
- 
-    let chiffre = document.querySelector(".form-num > input");
    
 
    
     if(event["inputType"] == "deleteContentBackward" || event["inputType"] == "deleteWordBackward"){
         let nouvelle_valeur = "";
-        let chiffre = document.querySelector(".form-num > input");
+        let chiffre = document.querySelector(debut);
         for(let i = 0; i <= chiffre.value.length ; i++){
             if(chiffre.value.charAt(i) != " "){
                 nouvelle_valeur += chiffre.value.charAt(i);
@@ -25,21 +57,24 @@ function handleReponse(event){
 
     if(chiffre_stock.length < 16){
         
-        let chiffre_carte = document.querySelector(".card-front-num p");
-        let chiffre = document.querySelector(".form-num > input");
+        let chiffre_carte = document.querySelector(arriver);
+        let chiffre = document.querySelector(debut);
         chiffre_stock += chiffre.value.charAt(chiffre.value.length - 1);
-        
         
         if((chiffre.getAttribute("class")).length % 4 == 0){
         }
 
         chiffre_carte.innerHTML = chiffre.value;
         let nouvelle_valeur = "";
-
+        chiffre_stock = document.querySelector(debut).value;
+        console.log(la_constante)
         for(let i = 0; i < 16; i++){
             if(i < chiffre_stock.length){
                 if(i%4 == 0 && i != 0){
-                    nouvelle_valeur += " ";
+                    if(la_constante === cart){
+
+                        nouvelle_valeur += " ";
+                    }
                     nouvelle_valeur += chiffre_stock.charAt(i);
                 }
                 else{
@@ -47,7 +82,15 @@ function handleReponse(event){
                 }
             }
             else{
-                nouvelle_valeur += cart.substring(i+Math.floor((nouvelle_valeur.length)/5),19);
+                console.log(la_constante);
+                if(la_constante === cart){
+                    
+                    nouvelle_valeur += la_constante.substring(i+Math.floor((nouvelle_valeur.length)/5),19);
+                    
+                }
+                else if(la_constante === secret){
+                    nouvelle_valeur += la_constante.substring(i,3);
+                }
                 break;
             }
         }
@@ -56,9 +99,6 @@ function handleReponse(event){
     else{
         
     }
-
-
-    
 }
 
 
