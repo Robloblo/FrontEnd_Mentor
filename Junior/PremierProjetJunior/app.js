756845895f32c505c6533ab1d05b93ebaa48b123
Robloblo <108494251+Robloblo@users.noreply.github.com>
2023-03-04 16:40:05 +0100

    javascript ça marche enfin a peux près

--- Junior/PremierProjetJunior/app.js
+++ Junior/PremierProjetJunior/app.js
@@ -1,13 +1,65 @@
 let chiffre = document.querySelector(".form-num > input");
+var chiffre_stock = "";
 
-chiffre.addEventListener('click',function(){
-   chiffre.addEventListener("keydown",function(){
-    let chiffre_carte = document.querySelector(".card-front-num p");
-    chiffre_carte.innerHTML = chiffre.value;
-   });
+const cart = "0000 0000 0000 0000";
+
+chiffre.addEventListener("input",handleReponse);
     
-    console.log(chiffre);
+function handleReponse(event){
+ 
+    let chiffre = document.querySelector(".form-num > input");
+   
 
-    console.log(chiffre_carte);
+   
+    if(event["inputType"] == "deleteContentBackward" || event["inputType"] == "deleteWordBackward"){
+        let nouvelle_valeur = "";
+        let chiffre = document.querySelector(".form-num > input");
+        for(let i = 0; i <= chiffre.value.length ; i++){
+            if(chiffre.value.charAt(i) != " "){
+                nouvelle_valeur += chiffre.value.charAt(i);
+            }
+            
+        chiffre_stock = nouvelle_valeur.substring(0,nouvelle_valeur.length-1);
+        }
+    }
 
-});
+    if(chiffre_stock.length < 16){
+        
+        let chiffre_carte = document.querySelector(".card-front-num p");
+        let chiffre = document.querySelector(".form-num > input");
+        chiffre_stock += chiffre.value.charAt(chiffre.value.length - 1);
+        
+        
+        if((chiffre.getAttribute("class")).length % 4 == 0){
+        }
+
+        chiffre_carte.innerHTML = chiffre.value;
+        let nouvelle_valeur = "";
+
+        for(let i = 0; i < 16; i++){
+            if(i < chiffre_stock.length){
+                if(i%4 == 0 && i != 0){
+                    nouvelle_valeur += " ";
+                    nouvelle_valeur += chiffre_stock.charAt(i);
+                }
+                else{
+                    nouvelle_valeur += chiffre_stock.charAt(i);
+                }
+            }
+            else{
+                nouvelle_valeur += cart.substring(i+Math.floor((nouvelle_valeur.length)/5),19);
+                break;
+            }
+        }
+        chiffre_carte.innerHTML = nouvelle_valeur; 
+    }
+    else{
+        
+    }
+
+
+    
+}
+
+
+
